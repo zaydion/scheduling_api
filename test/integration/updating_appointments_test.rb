f94c8def93ec95d33bbfc06c0f964c47ba6f6a41
cartagena <jcartagena93@gmail.com>
2016-01-10 17:06:09 -0500

    creates test for updating an appointment(test red)

---
+++ test/integration/updating_appointments_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class UpdatingAppointmentsTest < ActionDispatch::IntegrationTest
+  setup { @appointment = Appointment.create!(first_name: "matt", last_name: "wayne", date: "09/19/16",
+                                              start_time: "11:00", end_time: "11:05") }
+
+  test "updates apppointment" do
+    patch "/appointments/#{@appointment.id}"
+      { appointment: { first_name: "mark" } }.to_json
+      { 'Accept' => Mime::JSON, 'Content-Type' => Mime::JSON.to_s }
+
+      assert_equal 200, response.status
+      assert_equal 'mark', @appointment.reload.first_name
+  end
+end
